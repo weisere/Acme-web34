fcd84f44cccd9ef34b0b19f880ed7107e0bab912
Elyah Weiser <eweiser@student.touro.edu>
2023-04-21 16:33:01 -0400

    Story 3. Created the DecoratorDetailFacade that gets all the system status information that our various decorators need. So the information is obtainable through a centralized,                                           simple class, so that new programmers can easily see what status items we care about and how to obtain them

---
+++ src/main/java/com/acme/statusmgr/beans/DecoratorDetailFacade.java
@@ -0,0 +1,28 @@
+package com.acme.statusmgr.beans;
+
+/**
+ * This class is a facade all the varied types of system status we make use of. So the information is obtainable through a centralized,
+ * simple class, so that new programmers can easily see what status items we care about and how to obtain them
+ */
+public class DecoratorDetailFacade {
+    public long getAvailableProcessors(){
+        return Runtime.getRuntime().availableProcessors();
+    }
+
+    public long getFreeJVMMemory(){
+        return Runtime.getRuntime().freeMemory();
+    }
+
+    public Runtime.Version getJreVersion(){
+        return Runtime.version();
+    }
+
+    public String getTempLocation(){
+        return System.getenv("TEMP");
+    }
+
+    public long getTotalJVMMemory(){
+        return Runtime.getRuntime().totalMemory();
+    }
+
+}
