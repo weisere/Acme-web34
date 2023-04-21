aba565588783746d29548498343223983e2d8c79
Elyah Weiser <eweiser@student.touro.edu>
2023-04-21 17:55:46 -0400

    Story 4. Made the facade into type SystemStatusInterface so can now take either mock or real data. Also added a method to set the facade to which one we want

--- src/main/java/com/acme/statusmgr/beans/decoratorDetails/BaseDecorator.java
+++ src/main/java/com/acme/statusmgr/beans/decoratorDetails/BaseDecorator.java
@@ -1,7 +1,11 @@
 package com.acme.statusmgr.beans.decoratorDetails;
 
-import com.acme.statusmgr.beans.DecoratorDetailFacade;
+
 import com.acme.statusmgr.beans.ServerInterface;
+import com.acme.statusmgr.beans.SystemStatuses.SystemStatusInterface;
+import com.acme.statusmgr.beans.SystemStatuses.mockSystemStatus;
+import com.acme.statusmgr.beans.SystemStatuses.realSystemStatus;
+
 /**
  * This abstract class allows for easy creation of multiple decorators
  * of the type ServerInterface which StatusServer is also of that type
@@ -9,7 +13,12 @@ import com.acme.statusmgr.beans.ServerInterface;
  */
 public abstract class BaseDecorator implements ServerInterface {
     ServerInterface ss;
-    DecoratorDetailFacade ddf = new DecoratorDetailFacade();
+    static SystemStatusInterface sSI = new realSystemStatus();
+
+
+    public static void setSystemInfoFacade(SystemStatusInterface ssi) {
+        sSI = ssi;
+    }
 
     public BaseDecorator(ServerInterface ss){this.ss = ss;}
 
