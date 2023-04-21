5188fbd9b71b17a511bf4edab1969040185971aa
Elyah Weiser <eweiser@student.touro.edu>
2023-04-21 16:04:53 -0400

    Story 2. The way I made the decorators none of the information is stored and then retrieved when the method is called. Every time details are asked for it re-calls the method getting the most up-to-date information by calling the saved ServerStatus getDetails() and then adds the additional information requested in the decorator

--- src/main/java/com/acme/statusmgr/beans/decoratorDetails/BaseDecorator.java
+++ src/main/java/com/acme/statusmgr/beans/decoratorDetails/BaseDecorator.java
@@ -4,6 +4,7 @@ import com.acme.statusmgr.beans.ServerInterface;
 /**
  * This abstract class allows for easy creation of multiple decorators
  * of the type ServerInterface which StatusServer is also of that type
+ *
  */
 public abstract class BaseDecorator implements ServerInterface {
     ServerInterface ss;
