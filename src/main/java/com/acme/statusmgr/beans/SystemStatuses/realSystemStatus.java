67a6a9ad67982f2ae56fe15d91815849b195a576
Elyah Weiser <eweiser@student.touro.edu>
2023-04-21 17:57:46 -0400

    Story 4. Now implement the SystemStatusInterface

--- src/main/java/com/acme/statusmgr/beans/SystemStatuses/realSystemStatus.java
+++ src/main/java/com/acme/statusmgr/beans/SystemStatuses/realSystemStatus.java
@@ -1,10 +1,10 @@
-package com.acme.statusmgr.beans;
+package com.acme.statusmgr.beans.SystemStatuses;
 
 /**
  * This class is a facade all the varied types of system status we make use of. So the information is obtainable through a centralized,
  * simple class, so that new programmers can easily see what status items we care about and how to obtain them
  */
-public class DecoratorDetailFacade {
+public class realSystemStatus implements SystemStatusInterface {
     public long getAvailableProcessors(){
         return Runtime.getRuntime().availableProcessors();
     }
